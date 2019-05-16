96b1c8a82126a9f28f30fd9c0e8e2f6d0324e3ee
RiccardoOmiccioli <48404070+RiccardoOmiccioli@users.noreply.github.com>
2019-05-16 10:46:48 +0200

    Add files via upload

---
+++ StampaAlfabeto/src/StampaAlfabeto.java
@@ -0,0 +1,9 @@
+public class StampaAlfabeto {
+	public static void main( String [] args ) {
+		
+		char carattere = 'a';
+		
+		for( int i = 0; i < 26; i++) System.out.print( carattere++ + " " );
+	
+	}
+}
